55f67ea196b92ab3456c02dd3cf0aa476e21fc19
shisheng4215 <shisheng4215@163.com>
2019-12-01 00:32:41 +0800

    init

--- WebApplication2019113001/Data/EcommerceContext.cs
+++ WebApplication2019113001/Data/EcommerceContext.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication2019113001.Data.Entites;
 
 namespace WebApplication2019113001.Data
 {
---
+++ WebApplication2019113001/Data/Entites/AppRole.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2019113001.Data.Entites
+{
+    public class AppRole:IdentityRole<int>
+    {
+        public AppRole()
+        {
+
+        }
+
+        public AppRole(string name)
+        {
+            Name = name;
+        }
+    }
+}
